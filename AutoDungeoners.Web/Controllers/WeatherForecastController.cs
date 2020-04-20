7bda4428dc9148687d81c950a07a3e878bd1c06c
nightblade9 <nightbladecodes@gmail.com>
2020-04-20 11:42:39 -0400

    Weather API uses `/api` route too

--- AutoDungeoners.Web/Controllers/WeatherForecastController.cs
+++ AutoDungeoners.Web/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 namespace AutoDungeoners.Web.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
