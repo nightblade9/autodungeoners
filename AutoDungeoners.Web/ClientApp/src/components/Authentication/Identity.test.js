7450372baa5d3341f926d351d2a78bd5b79dafa5
nightblade9 <nightbladecodes@gmail.com>
2020-05-04 15:26:13 -0400

    First couple of Jest/React unit tests.

--- AutoDungeoners.Web/ClientApp/src/components/Authentication/Identity.test.js
+++ AutoDungeoners.Web/ClientApp/src/components/Authentication/Identity.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import { Identity } from './Identity';
 
-it('App renders without crashing', async () => {
+it('Identity renders without crashing', async () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <MemoryRouter>
-      <App />
+      <Identity />
     </MemoryRouter>, div);
   await new Promise(resolve => setTimeout(resolve, 1000));
 });
