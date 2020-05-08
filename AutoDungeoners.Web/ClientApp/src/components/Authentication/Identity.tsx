52301407094b95b3b8b909b6229e1b9b72f5fe4c
nightblade9 <nightbladecodes@gmail.com>
2020-05-08 16:07:39 -0400

    Fix warnings. Maybe.

--- AutoDungeoners.Web/ClientApp/src/components/Authentication/Identity.tsx
+++ AutoDungeoners.Web/ClientApp/src/components/Authentication/Identity.tsx
@@ -3,7 +3,11 @@ import { NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 
-export function Identity(props) {
+interface TokenProps {
+  token:string;
+}
+
+export function Identity(props:TokenProps) {
   return (
     <NavLink tag={Link} className="text-dark" to="/">{ props.token != null ? "Hi, " + jwtDecode(props.token).email : "Unauthenticated" }</NavLink>
   )
