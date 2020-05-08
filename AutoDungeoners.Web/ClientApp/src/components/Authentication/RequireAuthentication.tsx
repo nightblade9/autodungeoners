822518b6033c36b8f04bc3588726fff2b1047d6f
nightblade9 <nightbladecodes@gmail.com>
2020-05-08 11:44:59 -0400

    RequireAuthentication: now in TypeScript

--- AutoDungeoners.Web/ClientApp/src/components/Authentication/RequireAuthentication.tsx
+++ AutoDungeoners.Web/ClientApp/src/components/Authentication/RequireAuthentication.tsx
@@ -3,8 +3,8 @@ import  { Redirect } from 'react-router-dom';
 
 /// Annotate this in your render views to force redirecting to login if the user is not authenticated
 export class RequireAuthentication extends Component {
-    static displayName = RequireAuthentication.name;
-    render() {
+    static displayName:string = RequireAuthentication.name;
+    render = () => {
         if (localStorage.getItem("userInfo") == null)
         {
             return  <Redirect  to="/login" />
